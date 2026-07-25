659bca8a940b23a82b80fc3bfb451d0c459390c5
tomarrohitsingh1593-art <tomarrohitsingh1593@gmail.com>
2026-07-25 12:44:16 +0530

    Delete js directory

--- js/hotfix.js
+++
@@ -1,7 +0,0 @@
-/* BGPS HOTFIX LAYER
-   Future small frontend fixes should be added here instead of rewriting index.html or app.js.
-   Keep this file loaded after app.js.
-*/
-'use strict';
-
-// Intentionally empty baseline.
